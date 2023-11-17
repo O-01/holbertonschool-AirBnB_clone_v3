71f985978b08c523c86be153210a358fee0be7e3
mrktptn <marktipton95@gmail.com>
2023-11-17 17:05:07 -0600

    added get method to filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models import storage
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -68,3 +69,24 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object based on cls and id"""
+        if cls in classes:
+            key = f'{cls}.{id}'
+            obj_dict = storage.all(classes[cls])
+            for item in obj_dict.keys():
+                if item == key:
+                    req_obj = storage.all()[item]
+            if req_obj is not None:
+                return req_obj
+            else:
+                return None
+        else:
+            return None
+
+    def count(self, cls=None):
+        """
+        counts number of objects of a given class,
+        or count of all objects if no class given
+        """
