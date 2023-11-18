c959b8324d4356b65a3050e644a43b1b03d34c47
mrktptn <marktipton95@gmail.com>
2023-11-18 12:19:36 -0600

    added api blueprint, flask now connecting, unsure if jsonify correct

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
-""" """
+"""json api"""
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+from os import environ, getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def remover(exception):
+    """remove current session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(
+        host=getenv('HBNB_API_HOST')
+        if 'HBNB_API_HOST' in environ else '0.0.0.0',
+        port=getenv('HBNB_API_PORT')
+        if 'HBNB_API_PORT' in environ else '5000',
+        threaded=True
+    )
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-""" """
+"""index module returns json status"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status')
+def status_index():
+    """returns json status"""
+    return jsonify(status="OK")
