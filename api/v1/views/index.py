5c7492e4cbc83d264457c308fd32c0820d5c2663
mrktptn <marktipton95@gmail.com>
2023-11-18 12:51:56 -0600

    added /stats endpoint to show counts of all amenities

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,39 @@
 """index module returns json status"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {
+    "Amenity": Amenity,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User
+}
 
 
 @app_views.route('/status')
 def status_index():
     """returns json status"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats')
+def stats():
+    """returns json stats for all objects"""
+    stats = {
+        "amenities": storage.count(Amenity),
+        "city": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(stats)
